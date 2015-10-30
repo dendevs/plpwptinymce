e5248af2c33c1f23d3395b1ea457d09c75d75f93
dendev <dendev@dendev.adn>
2015-10-30 23:07:33 +0100

    add service

--- src/Kernel.php
+++ src/Kernel.php
@@ -12,4 +12,34 @@ class Kernel
 		$this->_container = new Container();
 	}
 
+	// add
+	public function add_service_test()
+	{
+		$id = 'test';
+
+		$function = function( $c ){
+			$obj = new \stdClass;
+			$obj->value = 'test ok';
+			return $obj;
+		};
+
+		return $this->_add_service( $id, $function );
+	}
+
+	private function _add_service( $id, $function )
+	{
+		return $this->_container[$id] = $function;
+	}
+
+	// get
+	public function get_service_test()
+	{
+		return $this->_get_service( 'test' );
+	}
+
+	private function _get_service( $id )
+	{
+		return $this->_container[$id];
+	}
+
 }
--- tests/KernelTest.php
+++ tests/KernelTest.php
@@ -8,5 +8,14 @@ class KernelTest extends PHPUnit_Framework_TestCase
 		$object = new Kernel();
 		$this->assertInstanceOf( "DenDev\Plpkernel\Kernel", $object );
 	}
+
+	public function test_add_service()
+	{
+		$object = new Kernel();
+		$object->add_service_test();
+		$service_test = $object->get_service_test();
+		$this->assertEquals( 'test ok', $service_test->value );
+
+	}
 }
 
